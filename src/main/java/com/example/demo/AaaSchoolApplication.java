38f4590eecec22432613601cf0ac592d57a25764
darcoCL <*newsecond@outlook.com*>
2018-04-27 11:05:10 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/AaaSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaSchoolApplication.class, args);
+	}
+}
